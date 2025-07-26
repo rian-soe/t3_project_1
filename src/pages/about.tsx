5f2e52b076269fe79cc675bc61aec097c4b5405c
rian-soe <riansoe65@gmail.com>
2025-07-26 16:39:10 +0630

    Updated homepage with personal introduction

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <main className="p-4">
+        <h1 className="text-3xl font-bold text-blue-600">About This Project</h1>
+        <p className="mt-2 text-gray-700">This is my first T3 app using Next.js!</p>
+      </main>
+    );
+  }
+  
